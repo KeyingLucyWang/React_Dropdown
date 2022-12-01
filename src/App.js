20678189797276ed773d8184bfd2f35fa01f7cf7
Lucy Wang <keyingw@andrew.cmu.edu>
2022-12-01 01:30:52 -0500

    Add dropdown menu component to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
+import DropdownMenu from "./DropdownMenu.js"
+
 function App() {
+
+  const items_single_title = "Intelligent Search APIs";
+  const items_single = [
+    {displayText: "Web Search", value:""},
+    {displayText: "Custom Search", value:""},
+    {displayText: "Copyright Search", value:""},
+    {displayText: "NFT Search", value:""},
+    {displayText: "Contextual Search", value:""},
+  ];
+
+  const items_multi_title = "Content Tagging APIs";
+  const items_multi = [
+    {displayText: "Visual Moderation", value:""},
+    {displayText: "Text Moderation", value:""},
+    {displayText: "Audio Moderation", value:""},
+    {displayText: "Demographic", value:""},
+    {displayText: "Logo & Logo Location", value:""},
+    {displayText: "Visual Context", value:""},
+    {displayText: "OCR", value:""},
+    {displayText: "Speech-to-Text", value:""},
+    {displayText: "Translation", value:""},
+    {displayText: "AI-Generated Media Recognition", value:""},
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      
+      <div className="use-cases">
+        <div class="column"><DropdownMenu itemsTitle={items_single_title} items={items_single} isMulti={false} /></div>
+        <div class="column"><DropdownMenu itemsTitle={items_multi_title} items={items_multi} isMulti={true} /></div>
+      </div>
+      
     </div>
   );
 }
